c1856ae6e7865cb486efe64f0e97b2f808b33739
yugr <yuyguogrongr@gmail.com>
2017-01-18 01:39:48 +0800

    创建数据库和表

---
+++ app/src/main/java/com/yugrdev/weather/db/Province.java
@@ -0,0 +1,35 @@
+package com.yugrdev.weather.db;
+
+/**
+ * Created by yuygu on 2017/1/18.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
