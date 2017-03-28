fa445d12c61aab736b6104c8478156a8489fce73
yugr <yuyguogrongr@gmail.com>
2017-03-28 23:30:34 +0800

    增加后台自动更新 服务 修改图标

--- app/src/main/java/com/yugrdev/weather/WeatherActivity.java
+++ app/src/main/java/com/yugrdev/weather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.yugrdev.weather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.yugrdev.weather.gson.Forecast;
 import com.yugrdev.weather.gson.Weather;
+import com.yugrdev.weather.service.AutoUpdateService;
 import com.yugrdev.weather.util.HttpUtil;
 import com.yugrdev.weather.util.Utility;
 
@@ -162,6 +164,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather", responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         } else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
